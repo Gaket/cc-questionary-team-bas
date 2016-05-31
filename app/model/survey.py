8a54c7c221d2b1b822f15de8f3124a7e4dc266e9
Artur <artusletter@gmail.com>
2016-05-31 11:32:23 +0300

    add survey class

---
+++ app/model/survey.py
@@ -0,0 +1,9 @@
+from array import array
+
+class Survey:
+    def __init__(self, questionsAddr):
+        questions = getQuestions(questionsAddr)
+
+def getQuestions(questionsAddr):
+    # Parse JSON to get data and return list of qustions (childs of Question)
+    pass
