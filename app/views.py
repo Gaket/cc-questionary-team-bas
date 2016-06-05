8006a1f584f285ac50525f9935a6adc731ecad34
Slava Lukin <v.lukin@innopolis.ru>
2016-06-05 21:28:41 +0300

    base.html and extends

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from app.main.survey import Survey
 from flask import render_template
+from flask import request
 
 @app.route('/')
 def hello():
@@ -10,7 +11,7 @@ def hello():
 # @app.route('/')
 # def world():
 #     user = {'nickname': 'You' }
-#     return render_template("layout.html",
+#     return render_template("base.html",
 #                            title = 'World',
 #                            user = user)
 
@@ -20,7 +21,7 @@ def temp():
 
 @app.route('/results')
 def result():
-    return "Results charts"
+    return request.form.get('name_MySQL')
 
 # @app.route('/hash/<int:survey_hash>', methods=['POST'])
 # def send_result(survey_hash):
