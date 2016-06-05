1d8e2bd0e4b74e50900415b733bf003aa1caceb5
Bulat Zamilov <b.zamilov@innopolis.ru>
2016-06-06 00:36:04 +0300

    Add login

--- app/views.py
+++ app/views.py
@@ -2,6 +2,12 @@ from app import app
 from app.main.survey import Survey
 from flask import render_template
 from flask import request
+from flask import session
+from flask import redirect
+from flask import url_for
+import json
+import os
+
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
@@ -17,16 +23,6 @@ def hello():
             res.append(request.form.get(key))
         return render_template('results.html', results=res)
 
-# @app.route()
-# def process_question():
-#     pass
-
-# @app.route('/')
-# def world():
-#     user = {'nickname': 'You' }
-#     return render_template("base.html",
-#                            title = 'World',
-#                            user = user)
 
 @app.route('/results')
 def chart():
@@ -34,10 +30,24 @@ def chart():
     values = [1, 9, 3, 2]
     return render_template('results.html', values=values, labels=labels)
 
-# @app.route('/')
-# def result():
-#     return request.form.get('about DB')
 
-# @app.route('/hash/<int:survey_hash>', methods=['POST'])
-# def send_result(survey_hash):
-#     pass
+@app.route('/login', methods=['GET', 'POST'])
+def check_login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        if request.form['username'] == 'admin' and \
+                request.form['password'] == 'secret':
+            session['admin'] = True
+            return redirect(url_for('statistics.html'))
+
+
+@app.route('/statistics')
+def get_statistics():
+    if not session['admin']:
+        return redirect(url_for('login'))
+    else:
+        data = json.load(os.path.join('app',
+                                      'data',
+                                      'aggregated_data.json'))
+        return render_template('statistics.html', data=data)
