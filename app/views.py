f90f98a41a320bd588b4eae2877b7fdf07d41264
Slava Lukin <v.lukin@innopolis.ru>
2016-06-05 10:29:16 +0300

    Q1 loop

--- app/views.py
+++ app/views.py
@@ -4,8 +4,15 @@ from flask import render_template
 
 @app.route('/')
 def hello():
-    survey = Survey("app\data\questions.json")
+    survey = Survey("app/data/questions.json")
     return render_template('index.html', survey=survey)
+
+# @app.route('/')
+# def world():
+#     user = {'nickname': 'You' }
+#     return render_template("layout.html",
+#                            title = 'World',
+#                            user = user)
 
 @app.route('/temp')
 def temp():
