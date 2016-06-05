d5c7639d68ac388635e8f5845f3c667aed58dcc2
Bulat Zamilov <b.zamilov@innopolis.ru>
2016-06-05 18:44:50 +0300

    Add id and archived property

--- app/main/survey.py
+++ app/main/survey.py
@@ -1,30 +1,31 @@
 import json
 import time
+import hashlib
 from app.model.question import NumericQuestion
 from app.model.question import MultipleAnswersQuestion
 from app.model.question import OpenQuestion
+
 
 class Survey:
     def __init__(self, questionsAddr):
         self.hash = str(int(time.time())).__hash__()
         self.questions = getQuestions(questionsAddr)
 
+
 def getQuestions(questionsAddr):
     # Parse JSON to get data and return list of questions (childs of Question)
     with open(questionsAddr) as questions_file:
         questions = json.load(questions_file)
-        questions_list = []
         q = dict()
         for key in questions:
-            text = questions[key]["text"]
-            variants = questions[key]["variants"]
+            if questions[key]["archived"]:
+                pass
             if questions[key]["type"] == "num":
-                q[key] = NumericQuestion(text, variants)
+                q[key] = NumericQuestion(**questions[key])
             elif questions[key]["type"] == "mult":
-                q[key] = MultipleAnswersQuestion(text, variants)
+                q[key] = MultipleAnswersQuestion(**questions[key])
             elif questions[key]["type"] == "open":
-                q[key] = OpenQuestion(text)
+                q[key] = OpenQuestion(**questions[key])
             else:
                 print("Type error")
-            # questions_list.append(q)
     return q
--- app/model/question.py
+++ app/model/question.py
@@ -2,37 +2,28 @@ from array import array
 
 
 class Question:
-    def __init__(self):
-        id = -1
-        text = ''
+    def __init__(self, **params):
+        self.id = params['id']
+        self.text = params['text']
 
 
 class MultipleAnswersQuestion(Question):
-    variants = []
-
-    def __init__(self, text, variants):
-        self.text = text
+    def __init__(self, **params):
+        super().__init__(**params)
         self.type = 'mult'
-        self.variants = variants
+        self.variants = params['variants']
 
 
 class NumericQuestion(Question):
-    def __init__(self, text, min, max, step):
-        self.text = text
-        self.min = int(min)
-        self.max = int(max)
-        self.step = int(step)
-        self.type = 'num'
-
-    def __init__(self, text, values):
-        self.text = text
-        self.min = int(values[0])
-        self.max = int(values[1])
-        self.step = int(values[2])
+    def __init__(self, **params):
+        super().__init__(**params)
+        self.min = int(params['variants'][0])
+        self.max = int(params['variants'][1])
+        self.step = int(params['variants'][2])
         self.type = 'num'
 
 
 class OpenQuestion(Question):
-    def __init__(self, text):
+    def __init__(self, **params):
+        super().__init__(**params)
         self.type = 'open'
-        self.text = text
