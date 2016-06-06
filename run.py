e26bf64364056ec2b8e9d751da26b6b8090e9fd7
Bulat Zamilov <b.zamilov@innopolis.ru>
2016-06-06 14:42:17 +0300

    Add results_

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
 from app import app
-app.run(debug=True)
+import os
+app.secret_key = 'PWN3D'
+if "PORT" in os.environ:
+    port = os.environ["PORT"]
+else:
+    port = 5000
+app.run(port=port, debug=True)
