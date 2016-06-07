5f37d459a70d4c012b640dd4c40f5830ce38a8ae
Slava Lukin <v.lukin@innopolis.ru>
2016-06-07 10:26:19 +0300

    update

--- app/views.py
+++ app/views.py
@@ -59,7 +59,7 @@ def get_statistics():
         chart_labels = list()
         chart_values = list()
         for key, item in data.items():
-            if key == '1':
+            if questions[key].type == 'mult':
                 res[key] = dict()
                 chart_labels = []
                 chart_values = []
@@ -69,18 +69,25 @@ def get_statistics():
                 for val in item['answer']:
                     chart_values.append(float(val))
                 res[key]['vals'] = chart_values
-            elif key in ['2', '3']:
+            elif questions[key].type == 'num':
                 res[key] = dict()
                 chart_labels = []
                 chart_values = []
+                sum = 0
                 [chart_labels.append(x) for x in range(1, 11)]
                 res[key]['labels'] = chart_labels
                 for val in item['answer']:
                     chart_values.append(float(val))
+                    sum = sum + float(val)
                 res[key]['vals'] = chart_values
+                res[key]['avg'] = sum / len(chart_values)
             else:
-                pass
-        return render_template('statistics.html', data=res, lang=session['lang'], quest=questions)
+                res[key] = dict()
+                ans = list()
+                for val in item['answer']:
+                    ans.append(val)
+                res[key] = ans
+        return render_template('statistics.html', res=res, lang=session['lang'], quest=questions)
 
 
 @app.route('/lang', methods=["POST"])
