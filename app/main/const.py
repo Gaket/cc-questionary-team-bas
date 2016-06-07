35cb549953b0c610f7ed59222055b1465731752b
Artur Badretdinov <artursletter@gmail.com>
2016-06-07 14:15:40 +0300

    add questions

--- app/main/const.py
+++ app/main/const.py
@@ -1,4 +1,4 @@
 QUESTIONS_ADDR = "app/data/questions.json"
 AGGREGATED_ADDR = "app/data/aggregated_data.json"
 QUESTIONS_TO_SHOW = [1,2,3,6,7]
-QUESTIONS_TO_STATISTICS = [1,2,3,6,7]
+QUESTIONS_TO_STATISTICS = [1,2,3,4,5,6,7]
